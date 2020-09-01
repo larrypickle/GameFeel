ed80c0819e8421c715f734c6ba1ce0f034e2dfec
Larry Wu <larrywu@usc.edu>
2020-09-01 14:27:53 -0700

    Ball Explodey

---
+++ Assets/Scripts/ScreenFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFX : MonoBehaviour
+{
+    public GameObject myVFX;
+    // Start is called before the first frame update
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)//makes the effect when ball hits ground
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Hit");
+            gameObject.SetActive(false);
+            
+            GameObject spawnedObject = Instantiate(myVFX, transform.position, transform.rotation);
+            Destroy(spawnedObject, 5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnedObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //spawns ball at mouse location
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(spawnedObject, worldPosition, Quaternion.identity);
+        }
+    }
+}
