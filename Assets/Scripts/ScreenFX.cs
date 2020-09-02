99685ea9b2370bdc2cd53e7fcb99537f7b94f483
Ashutosh-Aryal <aryala@usc.edu>
2020-09-01 19:01:42 -0700

    Finished up prototype w/out sound

--- Assets/Scripts/ScreenFX.cs
+++ Assets/Scripts/ScreenFX.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class ScreenFX : MonoBehaviour
 {
     public GameObject myVFX;
+    public static bool didScore = false;
     // Start is called before the first frame update
-   
-
+    
     private void OnTriggerEnter2D(Collider2D collision)//makes the effect when ball hits ground
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             Debug.Log("Hit");
-            gameObject.SetActive(false);
-            
+
+            if(!didScore)
+            {
+                ShootBallScript.ResetTexts();
+            }
+
             GameObject spawnedObject = Instantiate(myVFX, transform.position, transform.rotation);
             Destroy(spawnedObject, 5f);
         }
--- Assets/Scripts/ShootBallScript.cs
+++ Assets/Scripts/ShootBallScript.cs
@@ -1,33 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShootBallScript : MonoBehaviour
 {
+    private const KeyCode resetBallButton = KeyCode.R;
+    private const KeyCode stopRotationButton = KeyCode.DownArrow;
     private const KeyCode rotateCCWButton = KeyCode.LeftArrow;
     private const KeyCode rotateCCButton = KeyCode.RightArrow;
     private const KeyCode shootButton = KeyCode.Space;
     
-    private const float POWER_ACCELERATION = 50.0f;
+    private const float POWER_ACCELERATION = 10.0f;
     private const float MIN_POWER = 0.0f;
-    private const float MAX_POWER = 100.0f;
-    private const float ANGULAR_ACCELERATION = 5.0f;
+    private const float MAX_POWER = 20.0f;
+    private const float MAX_FORCE_ANGLE = 2.0f;
+    private const float MIN_FORCE_ANGLE = -2.0f;
+    private const float ANGULAR_ACCELERATION = 2.0f;
 
     private float forceAngle = 0.0f;
     private float forceMagnitude = 0.0f;
     private float currentPosition = 0.0f;
     private float myCurrentPower = 0.0f;
 
+    private static int myCurrentScore = 0;
+    private int myHighScore = 0;
+
     private static bool canShoot = true;
     private bool isPowerAscending = true;
+    private bool isLastKeyCCW = false;
+
+    private Vector3 respawnLocation;
+
     private Transform arrowPivotTransform;
     private Vector3 arrowPivotRotation;
+    private Rigidbody2D myRigidBody;
+    private SpriteRenderer arrowRenderer;
+
+    private static Text scoreText;
+    private static Text highScoreText;
 
     [SerializeField] Slider powerSlider;
     [SerializeField] GameObject arrowPivotObject;
-    [SerializeField] GameObject triggerObject;
+    [SerializeField] GameObject arrowObject;
+    [SerializeField] GameObject scoreTriggerObject;
+    [SerializeField] GameObject scoreTextObject;
+    [SerializeField] GameObject highScoreTextObject;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] GameObject pointPrefab;
 
+    private GameObject[] points;
+    private const int MAX_POINTS = 20;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.gameObject != scoreTriggerObject)
+        {
+            return;
+        }
+
+        ScreenFX.didScore = true;
+        myCurrentScore += 1;
+
+        if(myHighScore <= myCurrentScore)
+        {
+            myHighScore = myCurrentScore;
+        }
+
+        SetTexts();
+        Debug.Log("Entered trigger enter!");
+    }
+
+    public static void ResetTexts()
+    {
+        scoreText.text = "Score: 0";
+        myCurrentScore = 0;
+    }
+
+    private void SetTexts()
+    {
+        scoreText.text = "Score: " + myCurrentScore;
+        highScoreText.text = "High Score: " + myHighScore;
+    }
     public static Vector2 RadianToVector2(float radian)
     {
         return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
@@ -42,51 +98,136 @@ public class ShootBallScript : MonoBehaviour
     {
         canShoot = bCanShoot;
     }
+
+    private Vector2 GetPointPosition(float time)
+    {
+        Vector2 normalizedDirection = DegreeToVector2(arrowPivotObject.transform.rotation.eulerAngles.z);
+        Vector2 initialPosition = (Vector2)transform.position;
+
+        Vector2 pointPosition = initialPosition + (normalizedDirection * myCurrentPower * time);
+        pointPosition += (0.5f * Physics2D.gravity * time * time);
+
+        return pointPosition;
+    }
     
     // Start is called before the first frame update
     void Start() {
         arrowPivotRotation = arrowPivotObject.transform.rotation.eulerAngles;
+        myRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        myRigidBody.isKinematic = true;
+        respawnLocation = gameObject.transform.position;
+        powerSlider.minValue = MIN_POWER;
+        powerSlider.maxValue = MAX_POWER;
+        arrowRenderer = arrowObject.GetComponent<SpriteRenderer>();
+
+        points = new GameObject[MAX_POINTS];
+
+        for(int x = 0; x < MAX_POINTS; x++)
+        {
+            points[x] = Instantiate(pointPrefab, transform.position, Quaternion.identity);
+        }
+
+        scoreText = scoreTextObject.GetComponent<Text>();
+        highScoreText = highScoreTextObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update() {
 
-        bool shotBall = false;
+        if(Input.GetKeyDown(resetBallButton))
+        {
+            gameObject.transform.position = respawnLocation;
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.isKinematic = true;
+            arrowRenderer.enabled = true;
+            ScreenFX.didScore = false;
+            return;
+        }
 
-        if(Input.GetKey(shootButton)) {
+        if(myCurrentPower > MAX_POWER)
+        {
+            myCurrentPower = MAX_POWER;
+        } else if(myCurrentPower < MIN_POWER)
+        {
+            myCurrentPower = MIN_POWER;
+        }
 
-            if(myCurrentPower == MAX_POWER)
-            {
+        powerSlider.value = myCurrentPower;
+
+        if (Input.GetKey(shootButton)) {
+
+            if (myCurrentPower == MAX_POWER) {
                 isPowerAscending = false;
-            } else if(myCurrentPower == MIN_POWER)
-            {
+            } else if (myCurrentPower == MIN_POWER) {
                 isPowerAscending = true;
             }
 
-            if(isPowerAscending) {
-
+            if (isPowerAscending) {
+                myCurrentPower += POWER_ACCELERATION * Time.deltaTime;
             } else
             {
-
+                myCurrentPower -= POWER_ACCELERATION * Time.deltaTime;
             }
 
+            for (int x = 0; x < MAX_POINTS; x++)
+            {
+                points[x].SetActive(true);
+                points[x].transform.position = GetPointPosition(x * 0.1f);
+            }
+        }
+
+        if(Input.GetKeyUp(shootButton))
+        {
+            myRigidBody.isKinematic = false;
+            Vector2 velocity = DegreeToVector2(arrowPivotObject.transform.rotation.eulerAngles.z);
+            velocity *= myCurrentPower;
+            myRigidBody.velocity = velocity;
+            myCurrentPower = 0.0f;
+            powerSlider.value = MIN_POWER;
+            arrowRenderer.enabled = false;
+
+            for(int x = 0; x < MAX_POINTS; x++)
+            {
+                points[x].SetActive(false);
+            }
+
+            return;
         }
 
         if(Input.GetKey(rotateCCButton))
         {
-            forceAngle -= ANGULAR_ACCELERATION;
+            if(isLastKeyCCW)
+            {
+                forceAngle = 0.0f;
+            }
+            isLastKeyCCW = false;
+
+            if (forceAngle > MIN_FORCE_ANGLE)
+            {
+                forceAngle -= ANGULAR_ACCELERATION * Time.deltaTime;
+            }
         }
 
         if(Input.GetKey(rotateCCWButton))
         {
-            forceAngle += ANGULAR_ACCELERATION;
+            if(!isLastKeyCCW)
+            {
+                forceAngle = 0.0f;
+            }
+            isLastKeyCCW = true;
+
+            if (forceAngle < MAX_FORCE_ANGLE)
+            {
+                forceAngle += ANGULAR_ACCELERATION * Time.deltaTime;
+            }
+        }
+
+        if(Input.GetKeyDown(stopRotationButton))
+        {
+            forceAngle = 0.0f;
         }
 
         arrowPivotObject.transform.Rotate(0.0f, 0.0f, forceAngle);
 
-        if(shotBall) {
-            canShoot = false;
-        }
-        
     }
 }
